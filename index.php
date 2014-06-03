16016a99dc5bf70d5d6bb3a2a0e7e3195c7bacf2
kdotchristy <kdotchristy@users.noreply.github.com>
2014-06-02 22:10:44 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require 'gallery.php';
+
+$gallery = new Gallery();
+$gallery->setPath('img/crown-suites');
+$images = $gallery->getImages(array('jpg'));
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Image Gallery</title>
+        <link rel="stylesheet" href="css/gallery.css">
+    </head>
+    <body>
+        <div class="container">
+            <?php if($images): ?>
+                <div class="gallery cf">
+                    <?php foreach($images as $image): ?>
+                        <div class="gallery-item">
+                            <a href="<?php echo $image['full']; ?>">
+                                <img alt="" src="<?php echo $image['thumbs']; ?>">
+                            </a>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php else: ?>
+                There are no Images.
+            <?php endif; ?>
+        </div>
+    </body>
+</html>
